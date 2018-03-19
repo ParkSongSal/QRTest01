ff52c4ca123c4f94f81c5496dad1f2b8f0ed214e
user <tkdanr2427@naver.com>
2018-03-19 23:43:47 +0900

    QRApp

---
+++ app/src/main/java/com/example/user/qr/CheckList.java
@@ -0,0 +1,13 @@
+package com.example.user.qr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_list);
+    }
+}
